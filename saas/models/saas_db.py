63678b91991ac5cd3de13433f8e60a26caaee0f2
Bruce Nguyễn <trungthanh.nguyen@dnpcorp.vn>
2020-10-25 15:21:30 +0700

    each db is each cr cursor

--- saas/models/saas_db.py
+++ saas/models/saas_db.py
@@ -63,9 +63,10 @@ class saas_db(models.Model):
             threaded_synchronization.start()
 
     def _auto_upgrade(self, db_name, models_name=[]):
-        db = sql_db.db_connect(db_name)
-        with api.Environment.manage(), db.cursor() as cr:
-            env = api.Environment(cr, SUPERUSER_ID, {})
-            _request_stack.push(None)
+        with api.Environment.manage():
+            new_cr = registry(self._cr.dbname).cursor()
+            env = api.Environment(new_cr, SUPERUSER_ID, {})
             env['ir.module.module'].sudo().search([('name', 'in', models_name)]).button_immediate_upgrade()
-            _request_stack.pop()
+            new_cr.commit()
+            new_cr.close()
+        return True
